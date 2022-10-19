a5e829f0bb7370a6419da3c81bef44c988f3ec9e
oguzhanaslann <oguz21199@gmail.com>
2022-10-19 07:31:05 +0300

    - workouts and reposts modules

--- app/src/main/java/com/oguzhanaslann/fittrack/ui/MainActivity.kt
+++ app/src/main/java/com/oguzhanaslann/fittrack/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.core.view.WindowCompat
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.oguzhanaslann.commonui.Navigator
 import com.oguzhanaslann.commonui.activityViewBinding
 import com.oguzhanaslann.fittrack.R
@@ -60,6 +61,7 @@ class MainActivity : AppCompatActivity() {
 
             Handler(Looper.getMainLooper()).postDelayed({
                 it.remove()
+                binding.bottomNavigationView.setupWithNavController(navController)
             }, 100)
         }
 
